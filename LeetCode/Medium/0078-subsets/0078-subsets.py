f0b7fee8a98b14e584cd2734fb058cc7d893f571
HyeonSoo <97567615+Richter3766@users.noreply.github.com>
2025-07-02 08:58:40 +0900

    Time: 4 ms (4.47%), Space: 17.8 MB (65.15%) - LeetHub

---
+++ LeetCode/Medium/0078-subsets/0078-subsets.py
@@ -0,0 +1,27 @@
+import copy
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        power set 만들기
+
+        백트래킹으로 충분히 구현 가능
+        """
+
+        answer = []
+
+        def backtracking(cur, idx):
+            answer.append(copy.deepcopy(cur))
+            if len(cur) == len(nums):
+                return
+
+            for i in range(idx, len(nums)):
+                num = nums[i]
+                if num not in cur:
+                    cur.append(num)
+                    backtracking(cur, i + 1)
+                    cur.pop()
+
+        backtracking([], 0)
+
+        return answer
