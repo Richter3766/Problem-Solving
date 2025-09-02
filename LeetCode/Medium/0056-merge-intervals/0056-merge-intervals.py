10a9b91cd8e4973c8f255b6185676ed120fb795a
HyeonSoo <97567615+Richter3766@users.noreply.github.com>
2025-09-02 09:17:04 +0900

    Time: 7 ms (72.92%), Space: 20.9 MB (89.35%) - LeetHub

---
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        answer = []
+
+        for interval in intervals:
+            if not answer:
+                answer.append(interval)
+                continue
+            
+            if answer[-1][1] >= interval[0]:
+                answer[-1][1] = max(answer[-1][1], interval[1])
+            else: answer.append(interval)
+        
+        return answer
